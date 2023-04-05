f0d4bb22bfb5653aaa46dcef1a87fa151472bcc0
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-04-06 03:04:29 +0800

    testtt

--- backend/model/authModel.js
+++ backend/model/authModel.js
@@ -6,6 +6,10 @@ const usersSchema = new Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
 })
 const Users = mongoose.model('users', usersSchema)
 
